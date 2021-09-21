70a4f8dd4423e4b25d1169a26b5f94f609c2df7e
thatjsprof <davidrums65@gmail.com>
2021-09-22 00:20:02 +0100

    fix: correct name of custom button folder

--- src/pages/componentsPreview/componentsPreview.tsx
+++ src/pages/componentsPreview/componentsPreview.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
-import CustomButton from '../../components/global/button/customButton';
+import CustomButton from '../../components/global/customButton/customButton';
 
 const ComponentsPreview = () => {
 	return (
