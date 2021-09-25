9a335be221e6bc94a98b3ef63a5a5a76c3b33269
David Ajayi <davidrums65@gmail.com>
2021-09-25 09:52:03 +0100

    Feat/hero section (#5)
    * feat: add error boundary, setup styling, add button component for test
    
    * refactor: make naming of components consistent
    
    * fix: correct name of css button module
    
    * fix: correct name of custom button folder
    
    * navbar implementation
    
    * create footer component file
    
    * feat: add some more custom style setup
    
    * links color change on hover
    
    * add social icons to footer
    
    * feat: font setup
    
    * feat: create hero section
    
    Co-authored-by: Samuel Adebayo <Samuel.Adebayo@ENTS-Adebayo.local>

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
 import React, { Fragment, lazy, Suspense } from 'react';
 import { Switch, Route } from 'react-router-dom';
-import './assets/app.scss';
+import './assets/scss/app.scss';
 import ErrorBoundary from './components/errors/errorBoundary/errorBoundary';
+import Footer from './components/footer/footer';
+import Navbar from './components/navbar/navbar';
 const ComponentsPreview = lazy(
 	() => import('./pages/componentsPreview/componentsPreview'),
 );
+const LandingPage = lazy(() => import('./pages/landingPage/landingPage'));
 
 const App = () => (
 	<Fragment>
+		<Navbar />
 		<Switch>
 			<ErrorBoundary>
 				<Suspense fallback={<div>Loading...</div>}>
-					<Route exact component={ComponentsPreview} path="/components"></Route>
+					<Route exact component={ComponentsPreview} path="/components" />
+					<Route exact component={LandingPage} path="/" />
 				</Suspense>
 			</ErrorBoundary>
 		</Switch>
+		<Footer />
 	</Fragment>
 );
 
--- src/pages/componentsPreview/componentsPreview.tsx
+++ src/pages/componentsPreview/componentsPreview.tsx
@@ -1,10 +1,65 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import CustomButton from '../../components/global/customButton/customButton';
+import CustomInput from '../../components/global/customInput/customInput';
 
 const ComponentsPreview = () => {
 	return (
-		<Container>
+		<Container className="mt-5">
+			<Row>
+				<h3 className="mb-5">
+					Inputs - Props (type ['The type of the Input'], placeholder, label
+					['The label of the input'])
+				</h3>
+				<Col>
+					<div className="mb-5">
+						<h4>Normal Input (sm)</h4>
+						<CustomInput
+							type="text"
+							placeholder="Input Field"
+							inputType="normal"
+							size="sm"
+							onChange={() => {}}
+						></CustomInput>
+
+						<h4>Normal Input (default size)</h4>
+						<CustomInput
+							type="text"
+							placeholder="Input Field"
+							inputType="normal"
+							onChange={() => {}}
+						></CustomInput>
+
+						<h4>Normal Input (lg)</h4>
+						<CustomInput
+							type="text"
+							placeholder="Input Field"
+							inputType="normal"
+							size="lg"
+							onChange={() => {}}
+						></CustomInput>
+					</div>
+				</Col>
+			</Row>
+			<Row>
+				<h3 className="mb-5">
+					Input Group - Props (type ['The type of the Input'], placeholder,
+					label ['The label of the input'])
+				</h3>
+				<Col>
+					<div className="mb-5">
+						<h4>Input Group</h4>
+						<CustomInput
+							type="text"
+							placeholder="Input Field"
+							inputType="group"
+							onChange={() => {}}
+						>
+							<CustomButton>Medium</CustomButton>
+						</CustomInput>
+					</div>
+				</Col>
+			</Row>
 			<Row>
 				<h3 className="mb-5">
 					Buttons - Props (variant ['The color of the button'], size ['The size
