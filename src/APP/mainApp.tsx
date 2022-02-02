ed5a2423ac51db1d07f727cfbe1833199a738421
Samuel Adebayo <sambayo28@gmail.com>
2022-02-02 04:22:39 +0100

    feature: dashboard

--- src/APP/mainApp.tsx
+++ src/APP/mainApp.tsx
@@ -1,21 +1,23 @@
 import React from 'react';
-import { useRouteMatch, Switch, Route, Redirect } from 'react-router-dom';
+import { useRouteMatch, Switch, Route } from 'react-router-dom';
 import './assets/scss/app.scss';
 import Login from './pages/login';
 import ResetPassword from './pages/reset-password';
 import ForgotPassword from './pages/forgot-password';
 import SignUp from './pages/signup';
+import Dashboard from './pages/dashboard';
 
 function MainApp() {
 	let { path } = useRouteMatch();
 	return (
 		<>
 			<Switch>
-				<Route
+				<Route exact path={`${path}`} component={Dashboard} />
+				{/* <Route
 					exact
 					path={`${path}`}
 					render={() => <Redirect to={`${path}/login`} />}
-				/>
+				/> */}
 				<Route component={Login} path={`${path}/login`} />
 				<Route component={SignUp} path={`${path}/signup`} />
 				<Route component={ResetPassword} path={`${path}/reset-password`} />
